be2eea1b50051e35294de3867bb661824eb84c2c
Ryan Gorsuch <ryan.gorsuch@echostar.com>
2017-09-22 11:02:57 -0600

    POST data to httpbin

--- hammer.go
+++ hammer.go
@@ -1,19 +1,27 @@
 package main
 
+import "bytes"
+import "encoding/json"
 import "fmt"
-import "time"
 import "github.com/google/uuid"
 import "github.com/davecgh/go-spew/spew"
+import "io/ioutil"
+import "net/http"
+import "time"
 
 type RenoMessage struct {
-    Event string
-    ID string
-    Timestamp int64
+    Event string    `json:"event"`
+    ID string       `json:"id"`
+    Timestamp int64 `json:"timestamp"`
 }
-
 
 func main() {
     fmt.Printf("hello, hammer")
-    m := RenoMessage{ "lsdvrs", uuid.New().String(), time.Now().Unix() }
+    m := &RenoMessage{ "lsdvrs", uuid.New().String(), time.Now().Unix() }
     spew.Dump(m)
+    js, _ := json.Marshal(m)
+    fmt.Println(string(js))
+    response, _ := http.Post("http://httpbin.org/post", "application/json", bytes.NewReader(js))
+    body, _ := ioutil.ReadAll(response.Body)
+    fmt.Println(string(body))
 }
