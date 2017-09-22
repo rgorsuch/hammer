c2c77b1c127a031029b62f99dec83e68fee148a1
Ryan Gorsuch <ryan.gorsuch@echostar.com>
2017-09-22 12:01:14 -0600

    Send messages to ReNo every 3 seconds. Uses RENO_URL from the environment

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Run the hammer like this.
 
 ```
 docker build -t hammer-app .
-docker run -it --rm --name hammer hammer-app
+docker run -it --rm -e RENO_URL --name hammer hammer-app
+OR
+docker build -t hammer-app . && docker run -e RENO_URL -it --rm --name hammer hammer-app
 ```
--- hammer.go
+++ hammer.go
@@ -4,9 +4,10 @@ import "bytes"
 import "encoding/json"
 import "fmt"
 import "github.com/google/uuid"
-import "github.com/davecgh/go-spew/spew"
 import "io/ioutil"
 import "net/http"
+import "os"
+import "sync"
 import "time"
 
 type RenoMessage struct {
@@ -15,13 +16,24 @@ type RenoMessage struct {
     Timestamp int64 `json:"timestamp"`
 }
 
+func send(url string) {
+    for {
+        m := &RenoMessage{ "lsdvrs", uuid.New().String(), time.Now().Unix() }
+        js, _ := json.Marshal(m)
+        fmt.Println("Sending json to ReNo: ", string(js))
+        response, _ := http.Post(url, "application/json", bytes.NewReader(js))
+        body, _ := ioutil.ReadAll(response.Body)
+        fmt.Println("    => ", string(body))
+        time.Sleep(3 * time.Second)
+    }
+}
+
 func main() {
+    url := os.Getenv("RENO_URL")
+    fmt.Println("Using RENO_URL: ", url)
     fmt.Printf("hello, hammer")
-    m := &RenoMessage{ "lsdvrs", uuid.New().String(), time.Now().Unix() }
-    spew.Dump(m)
-    js, _ := json.Marshal(m)
-    fmt.Println(string(js))
-    response, _ := http.Post("http://httpbin.org/post", "application/json", bytes.NewReader(js))
-    body, _ := ioutil.ReadAll(response.Body)
-    fmt.Println(string(body))
+    var wg sync.WaitGroup 
+    wg.Add(1)
+    go send(url)
+    wg.Wait()
 }
