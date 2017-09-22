968020343cc030f9451431c86d2dc1d760297ec8
Ryan Gorsuch <ryan.gorsuch@echostar.com>
2017-09-22 10:05:49 -0600

    First tiny Reno message with uuid and timestamp

--- hammer.go
+++ hammer.go
@@ -1,7 +1,19 @@
 package main
 
 import "fmt"
+import "time"
+import "github.com/google/uuid"
+import "github.com/davecgh/go-spew/spew"
+
+type RenoMessage struct {
+    Event string
+    ID string
+    Timestamp int64
+}
+
 
 func main() {
     fmt.Printf("hello, hammer")
+    m := RenoMessage{ "lsdvrs", uuid.New().String(), time.Now().Unix() }
+    spew.Dump(m)
 }
